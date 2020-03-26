c685b38404d925f408c90b8755e993b2f97f3467
ibra <ibralugma@gmail.com>
2020-03-26 11:10:58 -0400

    feat: 🎸 Connect to test db if environment is set to `TEST`
    ✅ Closes: BS-115

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub use bigdecimal::BigDecimal;
 pub use diesel;
 
+mod environment;
 mod types;
 
 pub mod executor;
@@ -10,8 +11,25 @@ pub use self::executor::{Executor, ExecutorRef};
 pub use self::result::DbError;
 pub use self::types::*;
 
+use environment::Environment;
+
+/// Gets database url from env
+///
+/// Either `DATABASE_URL` or combination of
+///   `DB_USER`, `DB_PASSWORD`, `DB_HOST`, `DB_DATABASE` envs must be set
+///
+/// When `ENVIRONMENT`=`TEST`
+///   expects user to set `DATABASE_TEST_URL` env
 pub fn get_env_database_url() -> String {
   use std::env;
+
+  if let Ok(env_type) = env::var("ENVIRONMENT") {
+    let env_type = Environment::new(env_type);
+    if env_type.is_test() {
+      return env::var("DATABASE_TEST_URL").expect("DATABASE_TEST_URL must be set");
+    }
+  }
+
   match env::var("DATABASE_URL").ok() {
     Some(url) => url,
     None => format!(
@@ -36,5 +54,15 @@ pub fn connect_env() -> DbConn {
   let database_url = get_env_database_url();
   let conn =
     DbConn::establish(&database_url).expect(&format!("Error connecting to {}", database_url));
+  conn
+}
+
+/// Starts test transaction
+///
+pub fn start_test_transaction(conn: DbConn) -> DbConn {
+  use diesel::prelude::*;
+  conn
+    .begin_test_transaction()
+    .expect("begin test transaction");
   conn
 }
