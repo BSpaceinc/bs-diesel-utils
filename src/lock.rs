87ef8a15cba04da4c2ca60fa0e245d18ee28e0a4
Flux Xu <fluxxu@gmail.com>
2020-08-21 08:29:00 -0400

    feat: generic advisory lock id.

--- src/lock.rs
+++ src/lock.rs
@@ -1,14 +1,25 @@
 use crate::DbConn;
 use diesel::prelude::*;
 
-pub fn transaction_with_advisory_lock<F, R, E>(conn: &DbConn, lock: i64, f: F) -> Result<R, E>
+pub trait AsAdvisoryLockID {
+  fn as_advisory_lock_id(&self) -> i64;
+}
+
+impl AsAdvisoryLockID for i64 {
+  fn as_advisory_lock_id(&self) -> i64 {
+    *self
+  }
+}
+
+pub fn transaction_with_advisory_lock<ID, F, R, E>(conn: &DbConn, lock_id: ID, f: F) -> Result<R, E>
 where
+  ID: AsAdvisoryLockID,
   F: FnOnce() -> Result<R, E>,
   E: From<diesel::result::Error>,
 {
   conn.transaction(|| {
     diesel::sql_query(r#"SELECT pg_advisory_xact_lock($1)"#)
-      .bind::<diesel::sql_types::BigInt, _>(lock)
+      .bind::<diesel::sql_types::BigInt, _>(lock_id.as_advisory_lock_id())
       .execute(conn)?;
 
     let res = f()?;
