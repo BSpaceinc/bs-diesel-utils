a503f25403e92e679c3f20491a542954202810a0
Flux Xu <fluxxu@gmail.com>
2021-11-08 14:55:32 -0500

    add `Backend` and `BoxedQuery`

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,5 @@
+pub type Backend = diesel::pg::Pg;
 pub type DbConn = diesel::pg::PgConnection;
 pub type ConnectionManager = diesel::r2d2::ConnectionManager<DbConn>;
 pub type Pool = diesel::r2d2::Pool<ConnectionManager>;
+pub type BoxedQuery<'a, Source> = <Source as diesel::query_dsl::methods::BoxedDsl<'a, Backend>>::Output;
